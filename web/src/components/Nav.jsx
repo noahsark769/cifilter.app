af05d64cce6f141e7dd337428fc7303781dee7e8
Noah Gilmore <noah.w.gilmore@gmail.com>
2018-11-25 17:30:25 -0800

    More nav styles

--- web/src/components/Nav.jsx
+++ web/src/components/Nav.jsx
@@ -3,19 +3,63 @@ import styled from 'styled-components';
 import Logo from './Logo';
 
 const LogoWrapper = styled.div`
-    display: block;
+    display: inline-block;
     width: 30px;
     height: 30px;
 `;
 
 const StyledNav = styled.nav`
     width: 100%;
+    display: flex;
+    flex-direction: horizontal;
+`;
+
+const Title = styled.div`
+    font-size: 24;
+    font-weight: bold;
+    font-family: 'Roboto', 'Open Sans', Helvetica, sans-serif;
+    display: inline-block;
+`;
+
+const LogoAndTitle = styled.div`
+    flex: 2;
+
+    display: flex;
+    flex-direction: horizontal;
+    align-items: center;
+`;
+
+const Other = styled.div`
+    flex: 10;
+
+    display: flex;
+    flex-direction: horizontal;
+    justify-content: flex-end;
+    align-items: center;
+`;
+
+const NavLink = styled.a`
+    color: #F5BD5D;
+    cursor: pointer;
+
+    &:hover {
+        text-decoration: underline;
+    }
 `;
 
 const Nav = (props) => {
     return (
         <StyledNav className="padding--md">
-            <LogoWrapper><Logo /></LogoWrapper>
+            <LogoAndTitle>
+                <LogoWrapper className="margin-right--sm">
+                    <Logo />
+                </LogoWrapper>
+                <Title>CIFilter.io</Title>
+            </LogoAndTitle>
+            <Other>
+                <NavLink className="margin-right--md">ABOUT</NavLink>
+                <NavLink>SUPPORT</NavLink>
+            </Other>
         </StyledNav>
     )
 };
