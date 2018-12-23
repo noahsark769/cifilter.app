0942b35d9ddb892f591a9fbc4e1012613ea76c15
Noah Gilmore <noah.w.gilmore@gmail.com>
2018-12-23 16:16:19 -0600

    Look ma, even less collection view

--- iOS/CIFilter.io/Filter Workshop/ImageChooserView.swift
+++ iOS/CIFilter.io/Filter Workshop/ImageChooserView.swift
@@ -28,22 +28,6 @@ final class ImageChooserView: UIView {
         ControlEvent<UIImage>(events: chooseImageSubject)
     }()
 
-    private lazy var collectionView: UICollectionView = {
-        let view = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
-        view.backgroundColor = .clear
-        view.register(ImageChooserCell.self, forCellWithReuseIdentifier: String(describing: ImageChooserCell.self))
-        view.isScrollEnabled = false
-
-        guard let layout = view.collectionViewLayout as? UICollectionViewFlowLayout else {
-            fatalError("Should be the right layout type")
-        }
-        layout.minimumInteritemSpacing = artboardSpacing
-        layout.sectionInset = UIEdgeInsets(all: artboardPadding)
-        view.collectionViewLayout = layout
-
-        return view
-    }()
-
     init() {
         super.init(frame: .zero)
         self.backgroundColor = UIColor(rgb: 0xeeeeee)
@@ -76,35 +60,5 @@ final class ImageChooserView: UIView {
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-}
-
-extension ImageChooserView: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-        let image = UIImage(named: builtInImageNames[indexPath.row])!
-        self.chooseImageSubject.onNext(image)
-    }
-}
-
-extension ImageChooserView: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageChooserCell", for: indexPath) as! ImageChooserCell
-        cell.set(image: UIImage(named: builtInImageNames[indexPath.row])!)
-        return cell
-    }
-
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return builtInImageNames.count
-    }
-}
-
-extension ImageChooserView: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let size = (artboardSize - (artboardPadding * 2) - (artboardSpacing * 2)) / 3
-        return CGSize(width: size, height: size)
     }
 }
