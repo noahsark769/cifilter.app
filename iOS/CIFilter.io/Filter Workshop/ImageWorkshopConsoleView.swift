bbe4ee4f4d0ed9c0dae54b48b7291dbca14ecd18
Noah Gilmore <noah.w.gilmore@gmail.com>
2019-01-12 17:38:06 -0800

    try some more random shit

--- iOS/CIFilter.io/Filter Workshop/ImageWorkshopConsoleView.swift
+++ iOS/CIFilter.io/Filter Workshop/ImageWorkshopConsoleView.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
+extension CGPoint {
+    static func +(_ lhs: CGPoint, _ rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+}
+
 /**
  * A view which displays information about workshopping a filter to the user, including activity
  * inidcators and success/error messages.
  */
-final class ImageWorkshopConsoleView: UIStackView {
+final class ImageWorkshopConsoleView: UIView {
     enum EventType {
         case showActivity(animated: Bool)
         case hideActivity(animated: Bool)
@@ -24,12 +30,12 @@ final class ImageWorkshopConsoleView: UIStackView {
 
     init() {
         super.init(frame: .zero)
-        self.axis = .horizontal
-        self.spacing = 10
-        self.alignment = .fill
+//        self.axis = .horizontal
+//        self.spacing = 10
+//        self.alignment = .fill
 
-        self.addArrangedSubview(activityIndicator)
-        activityIndicator.isHidden = true
+        self.addSubview(activityIndicator)
+//        activityIndicator.isHidden = true
     }
 
     required init(coder: NSCoder) {
@@ -39,59 +45,75 @@ final class ImageWorkshopConsoleView: UIStackView {
     func update(for event: EventType) {
         switch event {
         case let .showActivity(animated):
-            let block: () -> Void = {
-                self.activityIndicator.isHidden = false
-            }
-            if animated {
-                UIView.animate(withDuration: 0.3) {
-                    block()
-                }
-            } else {
-                block()
-            }
             activityIndicator.startAnimating()
-        case let .hideActivity(animated):
-            let block = {
-                self.activityIndicator.isHidden = true
-            }
-            if animated {
-                UIView.animate(withDuration: 0.3) {
-                    block()
-                }
-            } else {
-                block()
-            }
-            activityIndicator.stopAnimating()
-        case let .success(message, animated):
-            let messageView = ImageWorkshopConsoleMessageView(type: .success, message: message)
-            if animated {
-                messageView.isHidden = true
-            }
-            self.addArrangedSubview(messageView)
-            if animated {
-                UIView.animate(withDuration: 0.3) {
-                    messageView.isHidden = false
-                }
-            }
-            let anim = CAKeyframeAnimation()
-            anim.isAdditive = true
+            activityIndicator.isHidden = false
+//            activityIndicator.layer.opacity = 0
+            activityIndicator.layoutIfNeeded()
+            let lastPosition = activityIndicator.layer.position
+            let offsetPosition = activityIndicator.layer.position + CGPoint(x: 40, y: 0)
 
-            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3, execute: {
-                if animated {
-                    UIView.animate(withDuration: 0.3, animations: {
-                        messageView.isHidden = true
-                    }, completion: { _ in
-                        self.removeArrangedSubview(messageView)
-                        messageView.removeFromSuperview()
-                    })
-                } else {
-                    self.removeArrangedSubview(messageView)
-                    messageView.removeFromSuperview()
-                }
-            })
+            let opacityAnimation = CABasicAnimation(keyPath: "opacity")
+            opacityAnimation.fromValue = 0
+            opacityAnimation.toValue = 1
+            opacityAnimation.duration = 1
+
+            let positionAnimation = CABasicAnimation(keyPath: "position")
+            positionAnimation.fromValue = offsetPosition
+            positionAnimation.toValue = lastPosition
+            positionAnimation.duration = 1
+
+            let group = CAAnimationGroup()
+            group.animations = [positionAnimation]
+
+            activityIndicator.layer.add(group, forKey: "trans")
+//            activityIndicator.layer.opacity = 1
+            activityIndicator.layer.position = lastPosition
+
+        case let .hideActivity(animated):
+//            let block = {
+//                self.activityIndicator.isHidden = true
+//            }
+//            if animated {
+//                UIView.animate(withDuration: 0.3) {
+//                    block()
+//                }
+//            } else {
+//                block()
+//            }
+            activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
+        case let .success(message, animated):
+            print("TPDO")
+//            let messageView = ImageWorkshopConsoleMessageView(type: .success, message: message)
+//            if animated {
+//                messageView.isHidden = true
+//            }
+//            self.addArrangedSubview(messageView)
+//            if animated {
+//                UIView.animate(withDuration: 0.3) {
+//                    messageView.isHidden = false
+//                }
+//            }
+//            let anim = CAKeyframeAnimation()
+//            anim.isAdditive = true
+//
+//            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3, execute: {
+//                if animated {
+//                    UIView.animate(withDuration: 0.3, animations: {
+//                        messageView.isHidden = true
+//                    }, completion: { _ in
+//                        self.removeArrangedSubview(messageView)
+//                        messageView.removeFromSuperview()
+//                    })
+//                } else {
+//                    self.removeArrangedSubview(messageView)
+//                    messageView.removeFromSuperview()
+//                }
+//            })
         case let .error(message):
-            let messageView = ImageWorkshopConsoleMessageView(type: .error, message: message)
-            self.addArrangedSubview(messageView)
+            print("TODO")
+//            let messageView = ImageWorkshopConsoleMessageView(type: .error, message: message)
+//            self.addArrangedSubview(messageView)
         }
     }
 }
