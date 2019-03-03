bcee05fb8945d222a44c4666cde65327b5ed8941
Noah Gilmore <noah.w.gilmore@gmail.com>
2019-03-03 13:23:42 -0800

    A more better example style

--- web/src/components/FilterExample.jsx
+++ web/src/components/FilterExample.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import FilterExampleImage from './FilterExampleImage';
 import FilterDetailSectionHeading from './FilterDetailSectionHeading';
-import { IoIosArrowRoundDown } from 'react-icons/io';
+import { IoIosArrowRoundForward } from 'react-icons/io';
 
 const Container = styled.div`
 
@@ -14,27 +14,18 @@ const ImageList = styled.div`
 `;
 
 const Wrapper = styled.div`
-    flex: 1;
+    flex: 4;
     &:first-child {
         margin-right: 20px;
     }
-`;
-
-const OutputImageWrapper = styled.div`
-    flex: 2;
-    &:first-child {
-        margin-right: 20px;
-    }
-`;
-
-const OutputImageSpacer = styled.div`
-    flex: 1;
 `;
 
 const ArrowContainer = styled.div`
+    flex: 1;
     display: flex;
+    flex-direction: column;
     justify-content: center;
-    margin: 20px 0;
+    margin: 0 20px;
 `;
 
 const FilterExample = (props) => {
@@ -58,19 +49,15 @@ const FilterExample = (props) => {
                             />
                         </Wrapper>);
                 })}
-            </ImageList>
-            <ArrowContainer>
-                <IoIosArrowRoundDown size={50} color="#999" />
-            </ArrowContainer>
-            <ImageList>
-                <OutputImageSpacer />
-                <OutputImageWrapper>
+                <ArrowContainer>
+                    <IoIosArrowRoundForward size={50} color="#999" />
+                </ArrowContainer>
+                <Wrapper>
                     <FilterExampleImage
                         name="outputImage"
                         filename={`${props.example.basepath}/${outputImageFilename}`}
                     />
-                </OutputImageWrapper>
-                <OutputImageSpacer />
+                </Wrapper>
             </ImageList>
         </Container>
     )
