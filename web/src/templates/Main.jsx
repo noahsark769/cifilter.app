e26bf5079aab30774184ba87aa6e955eb7623731
Noah Gilmore <noah.w.gilmore@gmail.com>
2019-03-31 13:44:29 -0700

    A bit of mobile handling

--- web/src/templates/Main.jsx
+++ web/src/templates/Main.jsx
@@ -22,13 +22,61 @@ const Container = styled.div`
     height: 100%;
 `;
 
+const MOBILE_STATE = {
+    LIST: "list",
+    DETAIL: "detail"
+}
+
 class Main extends React.Component {
-    state = { selectedFilter: null }
+    state = {
+        selectedFilter: null,
+        isMobile: false,
+        mobileState: MOBILE_STATE.LIST
+    }
+
+    handleWindowResize() {
+        console.log(`Window resize: ${window.innerWidth}`);
+        if (window.innerWidth < 600 && !this.state.isMobile) {
+            this.setState({ isMobile: true })
+        } else if (window.innerWidth >= 600 && this.state.isMobile) {
+            this.setState({ isMobile: false })
+        }
+    }
 
     handleFilterSelected(filterName) {
         // TODO: this iterates over 200 filters, make it a map up front :/
         let newFilter = this.props.pageContext.filters.filter((filter) => filter.name === filterName)[0];
-        this.setState({ selectedFilter: newFilter });
+        this.setState({
+            selectedFilter: newFilter,
+            mobileState: MOBILE_STATE.DETAIL
+        });
+    }
+
+    renderMobileStateContent() {
+        console.log(this.state.mobileState);
+        if (this.state.mobileState === MOBILE_STATE.LIST) {
+            return (<FilterSelect
+                filters={this.props.pageContext.filters}
+                onSelectFilter={this.handleFilterSelected.bind(this)}
+                className="margin-right--sm" />);
+        } else {
+            return (<FilterDetail filter={this.state.selectedFilter} />);
+        }
+    }
+
+    renderContent() {
+        if (this.state.isMobile) {
+            return (<Container>
+                {this.renderMobileStateContent()}
+            </Container>);
+        }
+        return (<Container>
+            <FilterSelect
+                filters={this.props.pageContext.filters}
+                onSelectFilter={this.handleFilterSelected.bind(this)}
+                className="margin-right--sm" />
+            {!this.state.isMobile && <FilterDetail filter={this.state.selectedFilter} />}
+        </Container>);
     }
 
     render() {
@@ -38,13 +86,7 @@ class Main extends React.Component {
                     <title>CIFilter Reference</title>
                 </Helmet>
                 <Nav />
-                <Container>
-                    <FilterSelect
-                        filters={this.props.pageContext.filters}
-                        onSelectFilter={this.handleFilterSelected.bind(this)}
-                        className="margin-right--sm" />
-                    <FilterDetail filter={this.state.selectedFilter} />
-                </Container>
+                {this.renderContent()}
             </OuterWrapper>
         );
     }
@@ -52,6 +94,9 @@ class Main extends React.Component {
     componentDidMount() {
         ReactGA.initialize('UA-136382558-1');
         ReactGA.pageview(window.location.pathname + window.location.search);
+
+        window.addEventListener('resize', this.handleWindowResize.bind(this))
+        this.handleWindowResize();
     }
 };
 export default Main;
